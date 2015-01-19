dba8396cc48d89f3867cc34376924566c1cabe66
Hannes Moser <hannes@impossiblearts.com>
2015-01-19 01:47:56 +0100

    clean dist before writing

--- bin/vup.js
+++ bin/vup.js
@@ -5,6 +5,7 @@
 process.title = 'vup';
 
 var program = require( 'commander' ),
+    rmdir   = require( 'rmdir' ),
     mkdir   = require( 'mkdirp' ),
     resolve = require( 'resolve' ).sync,
     path    = require( 'path' ),
@@ -17,7 +18,6 @@ try {
     vupfile = resolve( path.join( basedir, 'Vupfile' ), {basedir: basedir} );
     opts = require( vupfile );
 } catch (err) {
-    console.error( vupfile );
     console.error( 'Vupfile not found' );
     process.exit( 1 );
 }
@@ -30,24 +30,26 @@ program
     .command( 'publish <file> <output>' )
     .description( 'Transcode and publish given videofile' )
     .action( function( file, output ) {
-        mkdir( opts.dist, function( err ) {
-            if (err) {
-                return console.error( err );
-            }
-
-            vup.transcode( file, output, opts, function( err ) {
+        rmdir( opts.dist, function( err ) {
+            mkdir( opts.dist, function( err ) {
                 if (err) {
-                    console.error( err );
-                    process.exit( 1 );
+                    return console.error( err );
                 }
 
-                vup.publish( opts, function( err ) {
+                vup.transcode( file, output, opts, function( err ) {
                     if (err) {
-                        return console.error( err );
+                        console.error( err );
+                        process.exit( 1 );
                     }
 
-                    console.log( 'All done.' );
-                } )
+                    vup.publish( opts, function( err ) {
+                        if (err) {
+                            return console.error( err );
+                        }
+
+                        console.log( 'All done.' );
+                    } )
+                } );
             } );
         } );
     } );
